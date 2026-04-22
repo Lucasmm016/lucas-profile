52502502e60ff0f634c1e2407d5b9ce0889e8d1b
Lucas <lucasmm016@gmail.com>
2026-04-22 17:50:08 -0300

    style: substituído fonte principal do projeto para Inter;

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist_Mono, Inter } from 'next/font/google'
 
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
 
 import './globals.css'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
+const inter = Inter({
+	variable: '--font-inter',
 	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
 const geistMono = Geist_Mono({
@@ -28,7 +29,7 @@ export default function RootLayout({
 	return (
 		<html
 			lang="pt-BR"
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+			className={`${inter.variable} ${geistMono.variable} ${inter.className} h-full antialiased`}
 			suppressHydrationWarning
 		>
 			<body className="min-h-full flex flex-col">
