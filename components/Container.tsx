bdb128b2e2c4d400ce2af34031af0ab7dea1e63c
Lucas <lucasmm016@gmail.com>
2026-04-24 15:28:29 -0300

    style: ajusta hover e posição do evento onClick no componente Container para melhorar a interação;

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,8 +4,6 @@ import { useState } from 'react'
 
 import { cn } from '@/lib/utils'
 
-import { Button } from './ui/button'
-
 interface Props {
 	title: string
 	children: React.ReactNode
@@ -16,7 +14,13 @@ export function Container({ title, children, className }: Props) {
 	const [isExpanded, setIsExpanded] = useState(false)
 
 	return (
-		<div className="group relative w-full rounded-md border border-border overflow-hidden">
+		<div
+			onClick={() => setIsExpanded(true)}
+			className={cn(
+				'group relative w-full rounded-md border border-border overflow-hidden',
+				isExpanded ? 'cursor-default' : 'cursor-pointer',
+			)}
+		>
 			<div className="w-full flex items-center gap-2 border-b border-border p-2">
 				<div className="flex items-center gap-1">
 					<div className="size-3 bg-red-500 rounded-full" />
@@ -25,21 +29,23 @@ export function Container({ title, children, className }: Props) {
 				</div>
 				<span className="text-xs text-muted-foreground">{title}</span>
 			</div>
+
 			<div
 				className={cn(
-					'w-full min-h-10 p-2 bg-zinc-800 transition-[max-height] duration-(--spring-duration) ease-(--spring-easing)',
+					'relative w-full min-h-10 p-2 bg-zinc-800 transition-[max-height] duration-(--spring-duration) ease-(--spring-easing)',
 					isExpanded ? 'max-h-auto' : 'max-h-96 group-hover:max-h-104',
 					className,
 				)}
 			>
+				{!isExpanded && (
+					<div className="absolute inset-0 bg-black/10 pointer-events-none duration-300 opacity-0 group-hover:opacity-100" />
+				)}
 				{children}
 			</div>
 
 			{!isExpanded && (
-				<div className="absolute bottom-0 left-0 w-full flex justify-center p-0.5 bg-primary/30 backdrop-blur-xs">
-					<Button onClick={() => setIsExpanded(true)} size="lg" variant="ghost" className="w-full">
-						Ler mais...
-					</Button>
+				<div className="absolute bottom-0 left-0 w-full flex justify-center p-2 bg-primary/30 backdrop-blur-xs">
+					<span className="text-sm">Ler mais...</span>
 				</div>
 			)}
 		</div>
